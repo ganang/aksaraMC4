9441bbf6317e70d8b1cd0d22a3fea7d10432dbf6
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2020-10-07 13:19:54 +0700

    Initial Commit

---
+++ aksaraMC4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aksaraMC4
+//
+//  Created by Ganang Arief Pratama on 07/10/20.
+//  Copyright © 2020 aksara.id. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
