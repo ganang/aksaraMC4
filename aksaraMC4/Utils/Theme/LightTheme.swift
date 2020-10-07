fa664e85db793169e2b042a7521170d1d1b5e674
Naufal <naufalnafian@gmail.com>
2020-10-07 19:10:31 +0700

    Region Screen
    just the view

--- aksaraMC4/Utils/Theme/LightTheme.swift
+++ aksaraMC4/Utils/Theme/LightTheme.swift
@@ -11,8 +11,8 @@ import UIKit
 class LightTheme: ThemeProtocol {
     
     var backgroundColor: UIColor = UIColor.init(displayP3Red: 1/255, green: 0/255, blue: 17/255, alpha: 1)
-    var blueColor: UIColor = UIColor.init(displayP3Red: 36/255, green: 84/255, blue: 227/255, alpha: 1)
-    var textColor1: UIColor = UIColor.init(displayP3Red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    var cardColor1: UIColor = UIColor.init(displayP3Red: 255/255, green: 255/255, blue: 255/255, alpha: 0.6)
+    var textColor1: UIColor = UIColor.init(displayP3Red: 29/255, green: 112/255, blue: 188/255, alpha: 1)
     var textColor2: UIColor = UIColor.init(displayP3Red: 197/255, green: 197/255, blue: 197/255, alpha: 1)
     var accentColor: UIColor = UIColor.init(displayP3Red: 24/255, green: 23/255, blue: 39/255, alpha: 1)
     var purpleColor: UIColor = UIColor.init(displayP3Red: 64/255, green: 20/255, blue: 128/255, alpha: 1)
