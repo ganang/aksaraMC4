771198f81c030aea990398d85210e3717d9da339
Ganang Arief Pratama <ganang.arief.p@gmail.com>
2020-10-07 14:07:11 +0700

    initial commit

---
+++ aksaraMC4/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  aksaraMC4
+//
+//  Created by Ganang Arief Pratama on 07/10/20.
+//  Copyright © 2020 aksara.id. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.init(name: "Now-Black", size: 18)
+        label.textColor = .blue
+        label.text = ""
+        
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.addSubview(textLabel)
+        textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+
+}
+
